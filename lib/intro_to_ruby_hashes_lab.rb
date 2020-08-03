a511fb5a372196893e3bbdef217a8ff19dd56d35
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-08-03 02:10:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,21 +3,16 @@ def my_hash_creator(key, value)
   key => value
 }
 my_hash_creator
+end
 
-end	end
+def read_from_hash(hash, key)
+  hash[key]
+end	
 
-
-def read_from_hash(hash, key)	def read_from_hash(hash, key)
-hash[key]
-  # return the correct value using the hash and key parameters	  # return the correct value using the hash and key parameters
-end	end
-
-
-
-def update_counting_hash(hash, key)	def update_counting_hash(hash, key)
+def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
   else
     hash[key] = 1
-  end
-  hash 
+end
+hash 
